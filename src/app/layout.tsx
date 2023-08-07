7f842f63d72f0c79960f5f0070a63512f55e94e1
Roman <ketzalec@gmail.com>
2023-08-07 15:50:36 +0300

    base struct

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,23 @@
-import './globals.css'
-import type {Metadata} from 'next'
+import "./globals.scss";
+import type { Metadata } from "next";
 import Header from "@/Components/Header/Header";
 
 export const metadata: Metadata = {
-    title: 'Главная',
-    description: 'Главная страница банков',
-}
+  title: "Главная",
+  description: "Главная страница банков",
+};
 
-export default function RootLayout({children,}: { children: React.ReactNode }) {
-    return (
-        <html lang="ru">
-            <body>
-                <Header/>
-                {children}
-            </body>
-        </html>
-    )
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ru">
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
 }
