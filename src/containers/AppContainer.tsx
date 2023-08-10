26fb4cdbcd7a28b46651465f11374f0befcd4c70
Roman <ketzalec@gmail.com>
2023-08-10 12:15:42 +0300

    tailwind fix

--- src/containers/AppContainer.tsx
+++ src/containers/AppContainer.tsx
@@ -1,14 +1,16 @@
+"use client";
+
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { SessionProvider } from "next-auth/react";
 
 const AppContainer = ({ children }: { children: React.ReactNode }) => {
-  const client = new QueryClient();
-  return (
-    <QueryClientProvider client={client}>
-      <SessionProvider>{children}</SessionProvider>
-    </QueryClientProvider>
-  );
+	const client = new QueryClient();
+	return (
+		<QueryClientProvider client={client}>
+			<SessionProvider>{children}</SessionProvider>
+		</QueryClientProvider>
+	);
 };
 
 export default AppContainer;
