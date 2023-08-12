cfa473325da3e3771ec3ec3788c1e34e1789dff4
Zh31n <zh31n@mail.com>
2023-08-12 14:52:57 +0300

    add page credist rating

--- src/app/credits/rating/page.tsx
+++ src/app/credits/rating/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Metadata} from "next";
 import CreditRating from "@/screens/CreditRating/CreditRating";
+import data from "@/core/data";
 
 export const metadata: Metadata = {
     title: 'Кредитный рейтинг'
 }
 
-const Page = () => <CreditRating/>;
+const Page = () => <CreditRating data={data.CreditRating}/>;
 
 export default Page;
--- src/screens/CreditRating/CreditRating.tsx
+++ src/screens/CreditRating/CreditRating.tsx
@@ -3,24 +3,37 @@ import s from './CreditRating.module.scss';
 import PageWrapper from "@/containers/PageWrapper";
 import IntroCreditRating from "@/screens/CreditRating/components/IntroCreditRating/IntroCreditRating";
 import Image from "next/image";
+import for_what from '@/assets/icons/for_what_2.svg';
+import for_what_2 from '@/assets/icons/for_what_1.svg';
+import ForWhat from "@/screens/CreditRating/components/ForWhat/ForWhat";
+import HowItWorks from "@/components/HowItWorks/HowItWorks";
+import w_3 from "@/assets/icons/w_3.svg";
+import FrequentQuestions from "@/components/FrequentQuestions/FrequentQuestions";
 
-const CreditRating = () => {
+type workT = {
+    img: string
+    title: string
+    sub: string
+    text: string
+}
+type questT = {
+    title: string
+    text: string
+}
+type Props = {
+    data: {
+        worksData: workT[],
+        questData: questT[]
+    }
+}
+
+const CreditRating = ({data}: Props) => {
     return (
         <PageWrapper>
             <IntroCreditRating/>
-            <div className={s.forWhat}>
-                <div className={s.title}>Для чего нужен <mark>кредитный рейтинг?</mark></div>
-                <div className={s.items_cont}>
-                    <div className={s.item}>
-                        <Image />
-                        <div className={s.title}>Оцените свои шансы на получение кредита</div>
-                        <p className={s.sub}>
-                            Кредитный рейтинг - это балл, рассчитанный на основе кредитной истории заемщика. Чем выше
-                            балл, тем выше вероятность получить кредит.
-                        </p>
-                    </div>
-                </div>
-            </div>
+            <ForWhat/>
+            <HowItWorks title={'Как работает'} sub={' сервис'} items={data.worksData}/>
+            <FrequentQuestions title={'Частые вопросы'} items={data.questData}/>
         </PageWrapper>
     );
 };
