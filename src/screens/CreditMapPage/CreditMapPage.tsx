8b88e56170449d4ff7a3f776a1721f78b2c09369
Roman <ketzalec@gmail.com>
2023-08-12 14:02:52 +0300

    add credit cards 50% of page

--- src/screens/CreditMapPage/CreditMapPage.tsx
+++ src/screens/CreditMapPage/CreditMapPage.tsx
@@ -2,13 +2,17 @@ import Wrapper from "@/containers/Wrapper";
 import Navigation from "./components/Navigation/Navigation";
 import Bonus from "../../components/Bonus/Bonus";
 import Offers from "./components/Offers/Offers";
+import OffersMonth from "@/screens/CreditMapPage/components/OffersMonth/OffersMonth";
+import Mailing from "@/components/Mailing/Mailing";
 
 export default function CreditMapPage() {
-  return (
-    <Wrapper>
-      <Navigation />
-      <Bonus />
-      <Offers />
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <Navigation/>
+            <Bonus/>
+            <Offers/>
+            <OffersMonth/>
+            <Mailing/>
+        </Wrapper>
+    );
 }
