8f144ee3f22b232961b271c787a45bad287cef07
Zh31n <zh31n@mail.com>
2023-09-19 10:23:05 +0300

    add open for accrodion in frequent questions

--- src/components/QuestionItem/QuestionItem.tsx
+++ src/components/QuestionItem/QuestionItem.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+'use client'
+
+import React, {useState} from 'react';
 import s from './QuestionItem.module.scss';
 import Image from "next/image";
 import plus from '@/assets/icons/plus.svg';
+import krestik from '@/assets/icons/crestik.svg';
 
 type Props = {
     item: {
@@ -11,10 +14,23 @@ type Props = {
 }
 
 const QuestionItem = ({item}: Props) => {
+
+    const [open, setOpen] = useState<boolean>(false);
+    const toggleOpen = () => {
+        setOpen(!open)
+    }
     return (
-        <div className={s.item}>
-            <div className={s.title}>{item.title}</div>
-            <Image src={plus} alt='' />
+        <div className={open ? s.item_active : s.item} onClick={toggleOpen}>
+            <div className={s.info}>
+                <div className={s.title}>{item.title}</div>
+                <Image className={open ? s.plus_a : s.plus} src={plus} alt=''/>
+            </div>
+            <p className={s.text}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed sapien vitae
+                tortor vestibulum mollis nec non orci. Phasellus fringilla, odio nec suscipit varius, purus libero
+                bibendum lacus, non vulputate urna tortor eget libero. Etiam efficitur lacus libero, eget blandit libero
+                molestie ut.
+            </p>
         </div>
     );
 };
