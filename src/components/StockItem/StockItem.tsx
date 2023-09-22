c4ef818fac7599bad07ac3a863da27a9066f6bb2
Роман Нарожнов <rommka_83@mail.ru>
2023-09-22 19:38:15 +0300

    err corrected

--- src/components/StockItem/StockItem.tsx
+++ src/components/StockItem/StockItem.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import s from './StockItem.module.scss';
+import React from 'react'
+import s from './StockItem.module.scss'
 
 type Props = {
-    title:string
-    sup:string
+  title: string
+  sup: string
+  handleClick: () => void
 }
 
-const StockItem = ({title,sup}:Props) => {
-    return (
-        <div className={s.stock_item}>
-            <div className={s.title}>{title}</div>
-            <div className={s.sup}>{sup}</div>
-        </div>
-    );
-};
+const StockItem = ({ title, sup, handleClick }: Props) => {
+  return (
+    <div className={s.stock_item} onClick={() => handleClick()}>
+      <div className={s.title}>{title}</div>
+      <div className={s.sup}>{sup}</div>
+    </div>
+  )
+}
 
-export default StockItem;
+export default StockItem
