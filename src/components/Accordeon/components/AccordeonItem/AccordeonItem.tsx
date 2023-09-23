ced3d01b64fb3314d8bced2ea977324e2443a021
Zh31n <zh31n@mail.com>
2023-09-23 17:47:00 +0300

    add funcs for accordeon

--- src/components/Accordeon/components/AccordeonItem/AccordeonItem.tsx
+++ src/components/Accordeon/components/AccordeonItem/AccordeonItem.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
+'use client'
+
+import React, {useState} from 'react';
 import s from './AccordeonItem.module.scss';
 import arr from "@/assets/icons/arr_d_spec.svg";
 import Image from "next/image";
 
 type Props = {
-    numb:string
-    sub:string
-    text:string
+    numb: string
+    sub: string
+    text: string
 }
 
-const AccordeonItem = ({numb,sub,text}:Props) => {
+const AccordeonItem = ({numb, sub, text}: Props) => {
+
+    const [active, setActive] = useState<boolean>(false)
+    const handleActive = () => setActive(!active)
     return (
-        <div className={s.acc_item}>
+        <div className={s.acc_item} onClick={handleActive} style={{maxHeight: active && '250px'}}>
             <div className={s.info}>
                 <div>
                     <span>{numb}</span>
                     {sub}
                 </div>
-                <Image src={arr} alt={'стрелочка вниз'}/>
+                <Image className={active ? s.image_a : s.image} src={arr} alt={'стрелочка вниз'}/>
             </div>
-            <div className={s.text} style={{display: 'none'}}>{text}</div>
+            <div className={s.text}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum pellentesque sagittis dolor in ornare. Morbi vitae metus tempor, maximus ante vel, suscipit
+                libero. Vivamus sagittis nunc nec laoreet eleifend. P
+            </div>
         </div>
     );
 };
