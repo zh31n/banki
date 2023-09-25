dc4933bd467eb3e3b397de4fcd8c4c40746712fb
Роман Нарожнов <rommka_83@mail.ru>
2023-09-25 07:25:37 +0300

    change saving-account-page added

--- src/app/deposits/saving-accounts/page.tsx
+++ src/app/deposits/saving-accounts/page.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import SavingAccountsPage from "@/screens/SavingAccountsPage/SavingAccountsPage";
-import {Metadata} from "next";
-import data from "@/core/data";
+import React from 'react'
+import SavingAccountsPage from '@/screens/SavingAccountsPage/SavingAccountsPage'
+import { Metadata } from 'next'
+import data from '@/core/data'
 
 export const metadata: Metadata = {
-    title: 'Накопительные счета'
+  title: 'Накопительные счета',
 }
 
 export default async function SavingAccounts() {
-    return <SavingAccountsPage staticData={data.SavingAccounts}/>;
-};
+  return <SavingAccountsPage staticData={data.DepositsPage} />
+}
