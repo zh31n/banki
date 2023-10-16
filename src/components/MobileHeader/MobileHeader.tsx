40d5d18fca898ef663447b6893ec5015eab4a26d
zh31n <zh31n@mail.ru>
2023-10-16 13:59:54 +0300

    add fix for mobile

--- src/components/MobileHeader/MobileHeader.tsx
+++ src/components/MobileHeader/MobileHeader.tsx
@@ -5,6 +5,7 @@ import userImg from '@/assets/icons/User.svg';
 import Image from 'next/image';
 import MobileNav from '@/components/MobileHeader/MobileNav/MobileNav';
 import Link from 'next/link';
+import logo from '@/assets/icons/logo.png';
 
 const MobileHeader = () => {
   const [visible, setVisible] = useState<boolean>(false);
@@ -22,8 +23,8 @@ const MobileHeader = () => {
           <span></span>
           <span></span>
         </div>
-        <Link href={'/'} className={s.logo}>
-          Логотип
+        <Link href={'/'}>
+          <Image src={logo} className={s.logo} alt={'logo'}/>
         </Link>
         <Image alt={'icon'} priority src={userImg} width={20} height={20} />
       </header>
